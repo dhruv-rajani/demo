0f51b55b252245a77cbe2bb485393d76455a5356
workb <workbydr247@gmail.com>
2024-04-19 12:53:56 +0530

    remove some comments

--- lib/api/api_constant_url.dart
+++ lib/api/api_constant_url.dart
@@ -1,8 +1,4 @@
 class ApiConstant {
-  /// here you can write all the API
-  /// clone
-  /// dhvj
-  /// dfjd
   // static String baseUrl = "https://jsonplaceholder.typicode.com";
   static String baseUrl = "https://dummyjson.com/";
   static String endPoint = "/users";
