9bc5ca793cd4385cf582f4b97c3c43c15879d458
workb <workbydr247@gmail.com>
2024-04-19 10:26:16 +0530

    first  commit

---
+++ android/app/src/main/kotlin/com/example/flutter_topics/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_topics
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/api/api_home.dart
@@ -0,0 +1,70 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_topics/api/api_helper.dart';
+
+class ApiHome extends StatefulWidget {
+  const ApiHome({super.key});
+
+  @override
+  State<ApiHome> createState() => _ApiHomeState();
+}
+
+class _ApiHomeState extends State<ApiHome> {
+  // List<UsersDataModel>? dataModel = [];
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    ApiHelper.getData();
+    d();
+  }
+
+  d() async {
+    await Future.delayed(const Duration(seconds: 4), () {
+      log(" async d ");
+
+      /// num datatype can accept both int and double
+      // num x = 5;
+      // num a = 10.02;
+      // print(a);
+    });
+    Future.delayed(const Duration(seconds: 2), () {
+      log(" async after 2 second ");
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Api Demo"),
+      ),
+      body: FutureBuilder(
+        future: ApiHelper.getData(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            return ListView.builder(
+              itemCount: ApiHelper.modelData?.users.length,
+              itemBuilder: (context, index) {
+                final constUser = ApiHelper.modelData?.users[index];
+                return ListTile(
+                  title: Text(
+                    constUser!.address.coordinates.lat.toString(),
+                    style: const TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),
+                );
+              },
+            );
+          }
+        },
+      ),
+    );
+  }
+}
