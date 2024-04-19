5541832ecadd26c9a580239f5b78c0bcf4cfc98c
workb <workbydr247@gmail.com>
2024-04-19 13:12:01 +0530

    for testing purpose

--- lib/api/api_constant_url.dart
+++ lib/api/api_constant_url.dart
@@ -1,5 +1,5 @@
 class ApiConstant {
   // static String baseUrl = "https://jsonplaceholder.typicode.com";
   static String baseUrl = "https://dummyjson.com/";
-  static String endPoint = "/users";
+  static String endPoint = "/";
 }
