02f6386c486fe2ba5a3ac29634a459a796fdec9f
workb <workbydr247@gmail.com>
2024-04-19 11:13:24 +0530

    clone

--- lib/api/api_constant_url.dart
+++ lib/api/api_constant_url.dart
@@ -1,5 +1,6 @@
 class ApiConstant {
   /// here you can write all the API
+  /// clone
   // static String baseUrl = "https://jsonplaceholder.typicode.com";
   static String baseUrl = "https://dummyjson.com/";
   static String endPoint = "/users";
