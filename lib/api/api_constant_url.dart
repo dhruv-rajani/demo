bfe7ef38d1915ccb03ee8d14ed4fc887cfed12d5
workb <workbydr247@gmail.com>
2024-04-19 10:31:18 +0530

    todo text added

--- lib/api/api_constant_url.dart
+++ lib/api/api_constant_url.dart
@@ -1,4 +1,5 @@
 class ApiConstant {
+  /// here you can write all the API
   // static String baseUrl = "https://jsonplaceholder.typicode.com";
   static String baseUrl = "https://dummyjson.com/";
   static String endPoint = "/users";
