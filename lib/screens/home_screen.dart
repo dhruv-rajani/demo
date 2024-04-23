0e22ac8742a838d7dec8ccb4e2fe577fdffab828
dhruv <workbydr247@gmail.com>
2024-04-23 15:17:55 +0530

    check for patch

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -61,7 +61,7 @@ class _HomeScreenState extends State<HomeScreen> {
             child: Container(
               padding: const EdgeInsets.all(16.0),
               child: const Text(
-                'Header Section',
+                'HeaderSection',
                 style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
               ),
             ),
