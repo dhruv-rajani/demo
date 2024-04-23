f85cfdb2486bca4129c2c6763f0a60d0e8912092
dhruv <workbydr247@gmail.com>
2024-04-23 17:38:12 +0530

    assert add

---
+++ assets/i.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class I extends StatelessWidget {
+  const I({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
